2fd9f723a9b87bd7f08471ae28a8b4bea6f8b0ae
So-chiru <unleash9224@gmail.com>
2021-01-24 16:32:19 +0900

    Update polybar, i3

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-exec polybar example &
+exec polybar sochiru &
 
 # echo "Polybar launched..."
